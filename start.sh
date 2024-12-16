93957d08e0676b0ecd89b18800063c28378ea336
Tafadzwa Tazvitadza <tafadzwatazvitadza@gmail.com>
2024-12-16 15:03:14 +0200

    updates

--- start.sh
+++ start.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-# start.sh
 
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Set Flask environment variables
-export FLASK_APP=app.py
-export FLASK_ENV=production  # or development as needed
+# Activate virtual environment
+source venv/bin/activate
 
-# Run database migrations
+# Export environment variables
+export FLASK_APP=app.py
+export FLASK_ENV=development
+
+# Apply database migrations
 flask db upgrade
 
-# Start the Gunicorn server
-exec gunicorn app:app --timeout 120 --workers=3
+# Start the Flask server
+flask run
+
